ed5d38e4c55d9da25aa6390e6e3ebbce9066b8e8
GeberFinn <GeberFinn@users.noreply.github.com>
2018-07-20 20:02:07 +0300

    Add Walls, CameraController, PlayerController

---
+++ MainProject/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    public Transform target;
+    public float smoothSpeed = .3f;
+    Vector3 position;
+
+
+
+
+    void LateUpdate () {
+	    if (target.position.y > transform.position.y)
+        {
+            position.x = transform.position.x;
+            position.z = - 10f;
+            position.y = target.position.y;
+            transform.position = Vector3.Lerp(transform.position, position,smoothSpeed*Time.deltaTime );
+             
+        }	
+	}
+}
---
+++ MainProject/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float movementSpeed =10f;
+
+    float movement = 0f;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal")*movementSpeed;
+	}
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
