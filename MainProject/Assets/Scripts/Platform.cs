3c36fd1024dbdc8a68d603adb30fd3831f2439b3
GeberFinn <GeberFinn@users.noreply.github.com>
2018-07-21 15:24:05 +0300

    Rework DangPlatform

--- MainProject/Assets/Scripts/Platform.cs
+++ MainProject/Assets/Scripts/Platform.cs
@@ -12,12 +12,21 @@ public class Platform : MonoBehaviour {
         if (collision.relativeVelocity.y <= 0f)
         {
             Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            
 
-            if (rb != null)
+            if ((rb != null)&(gameObject.tag == "Platform"))
             {
                 Vector2 velocity = rb.velocity;
                 velocity.y = jumpForce;
                 rb.velocity = velocity;
+            }
+            if (gameObject.tag == "DangPlatform")
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = 0f;
+                rb.velocity = velocity;
+                Player pl = collision.gameObject.GetComponent<Player>();
+                pl.movementSpeed = 0f;
             }
         }
 
