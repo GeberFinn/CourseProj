98351a4dba6ba4d6175880b1cef424e7b9d726df
GeberFinn <GeberFinn@users.noreply.github.com>
2018-07-22 20:06:45 +0300

    Rework Menu

--- MainProject/Assets/Scripts/Bulb.cs
+++ MainProject/Assets/Scripts/Bulb.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class Bulb : MonoBehaviour {
 
-    private float constIntens = 5.0f;
-    private float intens;
-    private float TimeDown;
-
-
-	void Start () {
-        TimeDown = 1.0f;
-	}
+    //private float constIntens = 5.0f;
+    //private float intens;
+    //private float TimeDown;
+    public float duration = 3.0f;
+    public float midValue = 4.0f;
+    public float Range = 3f;
+ //   void Start () {
+ //       TimeDown = 1.0f;
+	//}
 	
-	// Update is called once per frame
-	void Update () {
-        if (gameObject.GetComponent<Light>().intensity != constIntens) gameObject.GetComponent<Light>().intensity = constIntens;
 
-        if (TimeDown > 0) TimeDown -= Time.deltaTime;
-        if (TimeDown < 0) TimeDown = 0;
-        if (TimeDown == 0)
-        {
-            intens = Random.Range(3.0f, 6.0f);
-            gameObject.GetComponent<Light>().intensity = intens;
-            TimeDown = Random.Range(1f, 2f);
-        }
+	void Update () {
+        //if (gameObject.GetComponent<Light>().intensity != constIntens) gameObject.GetComponent<Light>().intensity = constIntens;
+
+        //if (TimeDown > 0) TimeDown -= Time.deltaTime;
+        //if (TimeDown < 0) TimeDown = 0;
+        //if (TimeDown == 0)
+        //{
+        //    intens = Random.Range(3.0f, 6.0f);
+        //    gameObject.GetComponent<Light>().intensity = intens;
+        //    TimeDown = Random.Range(1f, 2f);
+        //}
+
+
+        float phi = Time.time / duration * 2f * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * Range + midValue;
+        gameObject.GetComponent<Light>().intensity = amplitude;
+
+
+
     }
 }
